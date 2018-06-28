a0722c33c833a5b7c8d0a494f5ea2b31da007bef
cjwebb <webb.c.92@gmail.com>
2018-06-28 02:05:53 -0500

    updated test dss for mac

--- tests/utils/test_dss.py
+++ tests/utils/test_dss.py
@@ -6,6 +6,8 @@ from tiflash.utils import dss
 
 if platform.system() == 'Windows':
     eclipsec = 'eclipsec.exe'
+elif platform.system() == 'Darwin':
+    eclipsec = "Ccstudio.app/Contents/MacOS/ccstudio"
 else:
     eclipsec = 'ccstudio'
 
@@ -13,16 +15,16 @@ else:
 class TestDSS():
 
     def test_find_dss(self, t_env):
-        expected = os.path.normpath(t_env['CCS_PATH'] +
+        expected = os.path.normpath(t_env['CCS_INSTALLS'][0] +
                                     "/eclipse/" + eclipsec)
 
-        result = dss.find_dss(t_env['CCS_PATH'])
+        result = dss.find_dss(t_env['CCS_INSTALLS'][0])
 
         assert result == expected
 
     def test_call_dss(self, t_env):
         expected = (True, '')
-        dss_path = os.path.normpath(t_env['CCS_PATH'] +
+        dss_path = os.path.normpath(t_env['CCS_INSTALLS'][0] +
                                     "/eclipse/" + eclipsec)
 
         result = dss.call_dss(dss_path, [])
