6b4544897b732d569fc83552a4358ea91f7410cf
cjwebb <webb.c.92@gmail.com>
2018-08-07 01:17:14 -0500

    implemented tests

--- tests/core/test_api_expression.py
+++ tests/core/test_api_expression.py
@@ -24,3 +24,22 @@ class TestExpressionApi():
                                 serno=device['serno'],
                                 connection=device['connection'],
                                 devicetype=device['devicetype'])
+
+    def test_expression_with_symbol_load(self, device):
+        """Tries using expression command with invalid C syntax"""
+
+        if 'symbol' not in device.keys() or \
+            'symbol_image' not in device.keys():
+            pytest.skip("No symbol image path or symbol name")
+
+        EXPRESSION = device['symbol']
+        SYMBOL_FILE = device['symbol_image']
+
+        result = core.evaluate(EXPRESSION,
+                            symbol_file=SYMBOL_FILE,
+                            serno=device['serno'],
+                            connection=device['connection'],
+                            devicetype=device['devicetype'])
+
+        assert result == '0'
+
