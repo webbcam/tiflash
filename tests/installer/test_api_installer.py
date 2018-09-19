8e1d6200c1a1518c51beff86cf1f20fa45885990
cjwebb <webb.c.92@gmail.com>
2018-09-19 18:22:21 -0500

    added installer tests

---
+++ tests/installer/test_api_installer.py
@@ -0,0 +1,9 @@
+import sys
+
+class TestAPIInstaller():
+
+    def test_import(self):
+        """Tests tiflash can be imported"""
+        import tiflash
+
+        assert ( "tiflash" in sys.modules.keys() )
