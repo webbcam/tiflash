7262a7fec2d761644766f04333834775ddc84b3a
cjwebb <webb.c.92@gmail.com>
2019-04-06 13:25:30 -0500

    implemented new dss utility

--- tests/utils/test_dss.py
+++ tests/utils/test_dss.py
@@ -1,33 +1,13 @@
 import os
-import platform
 import pytest
 
 from tiflash.utils import dss
 
 
-if platform.system() == 'Windows':
-    eclipsec = 'eclipsec.exe'
-elif platform.system() == 'Darwin':
-    eclipsec = "Ccstudio.app/Contents/MacOS/ccstudio"
-else:
-    eclipsec = 'ccstudio'
-
-
 class TestDSS():
+    def test_launch_server(t_env):
+        """Tests DebugServer process can be launched successfully."""
+        p, port = dss.launch_server(tenv["ccs-exe"], tenv["workspace"])
+        assert p.poll() is None
 
-    def test_find_dss(self, tenv):
-        expected = os.path.normpath(tenv['paths']['ccs'] +
-                                    "/eclipse/" + eclipsec)
-
-        result = dss.find_dss(tenv['paths']['ccs'])
-
-        assert result == expected
-
-    def test_call_dss(self, tenv):
-        expected = (True, '')
-        dss_path = os.path.normpath(tenv['paths']['ccs'] +
-                                    "/eclipse/" + eclipsec)
-
-        result = dss.call_dss(dss_path, [], timeout=60)
-
-        assert result == expected
+        p.terminate()
